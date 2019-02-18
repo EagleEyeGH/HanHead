1f5f4ed3389973d192df2a1ccbd2cf589a0fa186
marijnt031 <47228437+marijnt031@users.noreply.github.com>
2019-02-18 16:50:26 +0100

    upgradescene

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    //Make sure to attach these Buttons in the Inspector
+    public Button m_YourFirstButton, m_YourSecondButton, m_YourThirdButton, m_exit;
+
+    public int ammunitionpoints = 40;
+    public int zombiepoints;
+
+    void Start()
+    {
+        //Calls the TaskOnClick/TaskWithParameters/ButtonClicked method when you click the Button
+        m_YourFirstButton.onClick.AddListener(Item01Click);
+        m_YourSecondButton.onClick.AddListener(Item02Click);
+        m_YourThirdButton.onClick.AddListener(Item03Click);
+        m_exit.onClick.AddListener(exit);
+    }
+
+    void Item01Click()
+
+    {
+        if (zombiepoints >= ammunitionpoints)
+        {
+            Debug.Log("currentpoints = Zombiepoints - ammunitionpoints ?? updating the current zombie points adding ammunution");
+
+        }
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button ammunition!");
+    }
+
+    void Item02Click()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button! damage ");
+    }
+
+    void Item03Click()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button! life ");
+    }
+
+    void exit()
+    {
+        Debug.Log("make this return to scene main_menu");
+        
+       // CanvasObject.GetComponent<Canvas>().enabled = false;
+    }
+
+
+    
+}
---
+++ Assets/ShopScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopScript : MonoBehaviour
+{
+    public static string Item01;
+    public static string Item02;
+    public static string Item03;
+
+    public static int shopNumber;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shopNumber == 1)
+        {
+            Item01 = "Ammuniton upgrade ";
+            Item02 = "Damage Upgrade ";
+            Item03 = "More lifes ";
+
+        }
+
+    }
+}
