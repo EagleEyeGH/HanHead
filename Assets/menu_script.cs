f6fd0796a6b7275a5a535365184d5a2ac491a9a1
Don Duijnstee <donduijnstee@hotmail.nl>
2019-02-18 16:39:14 +0100

    added main menu

---
+++ Assets/menu_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu_script : MonoBehaviour
+{
+
+    public void onPlayClicked() {
+        SceneManager.LoadScene("HanHead");
+    }
+
+    public void onUpgradeClicked()
+    {
+        SceneManager.LoadScene("Upgrade");
+    }
+
+    public void onExitClicked()
+    {
+        Application.Quit();
+    }
+}
